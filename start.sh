853b1f81f3793fa05b28ea1c93f9bcf4014a5c5e
ISE <halo@praise.ml>
2021-05-28 14:23:56 +0000

    update

--- start.sh
+++ start.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
-
-echo 'extension=pdo.so' > php.ini
-for Module in $( ls lib/* )
-do
-	echo "extension=$Module" >> php.ini
-done
-
 LD_LIBRARY_PATH=lib lib/ld-linux-x86-64.so.2 ./php -c php.ini$@
